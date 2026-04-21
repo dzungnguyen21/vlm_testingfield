05224f3332462e7f22dfbfbb11fc50734da1fd1e
dzungnguyen21 <22022546@vnu.edu.vn>
2026-04-21 18:01:28 +0700

    Disable bitsandbytes explicit fix

--- utils/load_model.py
+++ utils/load_model.py
@@ -1,23 +1,18 @@
 import gc
 import torch
-from transformers import AutoProcessor, LlavaForConditionalGeneration, BitsAndBytesConfig
+from transformers import AutoProcessor, LlavaForConditionalGeneration
 
 if "model" in globals():
     del model  # noqa: F821
 gc.collect()
 torch.cuda.empty_cache()
 
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16
-)
-
 model_id = "llava-hf/llava-1.5-7b-hf"
 model = LlavaForConditionalGeneration.from_pretrained(
     model_id,
     torch_dtype=torch.float16,
-    quantization_config=quantization_config,
     device_map="auto",
+    quantization_config=None,  # Explictly refuse bitsandbytes auto-activation
     attn_implementation="eager",
 )
 processor = AutoProcessor.from_pretrained(model_id)
