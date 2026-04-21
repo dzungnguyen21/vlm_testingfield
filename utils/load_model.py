1077e2600eb875ac224f3c5bd3065d4e3b9f2080
dzungnguyen21 <22022546@vnu.edu.vn>
2026-04-21 17:50:42 +0700

    Use 4-bit quantization to prevent Colab Ram OOM

--- utils/load_model.py
+++ utils/load_model.py
@@ -1,16 +1,22 @@
 import gc
 import torch
-from transformers import AutoProcessor, LlavaForConditionalGeneration
+from transformers import AutoProcessor, LlavaForConditionalGeneration, BitsAndBytesConfig
 
 if "model" in globals():
     del model  # noqa: F821
 gc.collect()
 torch.cuda.empty_cache()
 
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.float16
+)
+
 model_id = "llava-hf/llava-1.5-7b-hf"
 model = LlavaForConditionalGeneration.from_pretrained(
     model_id,
     torch_dtype=torch.float16,
+    quantization_config=quantization_config,
     device_map="auto",
     attn_implementation="eager",
 )
